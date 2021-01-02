b70881050e59b9fdac14e0758d90d5ab88213299
Vo Pham Minh Tan <minhtan@Vos-MacBook-Pro-2.local>
2021-01-03 00:40:07 +0700

    finish js

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
 let newsArticles = [];
 const API_KEY = "7ad27e6d8c4b4c818cca946bf4dfac73";
 let SourceObj = {};
+let numOfArticle = 0;
+let url = "";
+let categories = [
+  "Business",
+  "Entertainment",
+  "General",
+  "Health",
+  "Science",
+  "Sports",
+  "Technology",
+];
 
-async function update(){
-    // let url = `http://newsapi.org/v2/everything?q=javscript&from=2020-11-30&sortBy=publishedAt&language=en&apiKey=${API_KEY}`;
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
-    const result = await fetch(url);
-    const data = await result.json();
-    console.log(data);
-    newsArticles = data.articles;
-    newsArticles = newsArticles.map(x => ({...x, Checked: true}));
-    console.log(newsArticles);
-    updateSourceObj();
-    
-    render();
-    renderCheckBoxArea();
-}
-function updateSourceObj(){
-    // expected: SourceObj = { BBC: 2, CNN: 1, NewYork: 3}
-    SourceObj = {};
-    newsArticles.map(article => {
-        let key = article.source.name;
-        if (SourceObj.hasOwnProperty(key)) {
-            SourceObj[key] += 1;
-        }
-        else{
-            SourceObj[key] = 1;
-        }
-    });
-    console.log(SourceObj);
+function updateSourceObj() {
+  // expected: SourceObj = { BBC: 2, CNN: 1, NewYork: 3}
+  SourceObj = {};
+  newsArticles.map((article) => {
+    let key = article.source.name;
+    if (SourceObj.hasOwnProperty(key)) {
+      SourceObj[key] += 1;
+    } else {
+      SourceObj[key] = 1;
+    }
+  });
+  console.log(SourceObj);
 }
 function renderArticleCard(article) {
   return `
@@ -43,45 +39,98 @@ function renderArticleCard(article) {
                 <a href="${article.url}" class="btn btn-primary">View Story</a>
             </div>
             <div class="card-footer">
-                <small class="text-muted">${moment(article.publishAt).startOf('hour').fromNow()}</small>
+                <small class="text-muted">${moment(article.publishAt)
+                  .startOf("hour")
+                  .fromNow()}</small>
             </div>
         </div>
     </div>
     `;
 }
 
-function render(){
-    let resultsArea = document.getElementById("results");
-    resultsArea.innerHTML = newsArticles
-                            .filter(article => article.Checked === true)
-                            .map(article => renderArticleCard(article)).join("\n");
+function render() {
+  let resultsArea = document.getElementById("results");
+
+  resultsArea.innerHTML = newsArticles
+    .filter((article) => article.Checked === true)
+    .map((article) => renderArticleCard(article))
+    .join("\n");
 }
-function renderCheckBox(key){
-    return`
+
+// belong to checkbox
+function renderCheckBox(key) {
+  return `
         <input type="checkbox" class="btn-check" checked onchange="toggleSource(event)" value="${key}" id="${key}">
         <label class="btn btn-outline-primary mb-2" for="${key}">${key} (${SourceObj[key]})</label>
-    `
+    `;
 }
-function renderCheckBoxArea(){
-    let checkBoxArea = document.getElementById("checkBox"); 
-    checkBoxArea.innerHTML = "";
-    for(let key in SourceObj){
-        checkBoxArea.innerHTML += renderCheckBox(key);
+
+function renderCheckBoxArea() {
+  let checkBoxArea = document.getElementById("checkBox");
+  checkBoxArea.innerHTML = "";
+  for (let key in SourceObj) {
+    checkBoxArea.innerHTML += renderCheckBox(key);
+  }
+}
+// belong to checkbox
+
+function toggleSource(event) {
+  //update newsArticles
+  newsArticles.forEach((article) => {
+    if (article.source.name === event.target.value) {
+      article.Checked = event.target.checked;
     }
+  });
+  render();
 }
-function toggleSource (event){
-    //update newsArticles
-    newsArticles.forEach(article => {
-        if(article.source.name === event.target.value){
-            article.Checked = event.target.checked;
-        }
-    });
-    render();
+//The user should see new stories related to the category he/she chose.
+function renderCategory() {
+  categories.map((category) => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&${category}&apiKey=${API_KEY}`;
+
+    document.getElementById(
+      "category"
+    ).innerHTML += `<button id="${category}" onclick="haha(event)" type="button" class="list-group-item list-group-item-action">${category}</button>`;
+    /* document.getElementById(`${category}`).addEventListener("click", haha);
+    console.log(document.getElementById(`${category}`)); */
+  });
 }
+
+function haha(event) {
+  let cat = event.target.id;
+  url = `https://newsapi.org/v2/top-headlines?country=us&category=${cat}&apiKey=${API_KEY}`;
+  console.log("haha");
+  console.log(url);
+  update();
+}
+//rendercategory=> button=> function onclick
+renderCategory();
+async function update() {
+  // let url = `http://newsapi.org/v2/everything?q=javscript&from=2020-11-30&sortBy=publishedAt&language=en&apiKey=${API_KEY}`;
+
+  if (url === "") {
+    url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+  }
+  console.log(url);
+  const result = await fetch(url);
+  const data = await result.json();
+  console.log(data);
+  newsArticles = data.articles;
+  numOfArticle = newsArticles.length;
+  console.log(numOfArticle);
+  newsArticles = newsArticles.map((x) => ({ ...x, Checked: true }));
+  console.log(newsArticles);
+  loadMore.addEventListener("click", updateMore);
+  updateSourceObj();
+  render();
+  renderCheckBoxArea();
+
+  document.getElementById("numOfArticle").innerHTML = ` (${numOfArticle})`; //load number of article
+}
+
 update();
 
 let loadMore = document.getElementById("loadMore");
-loadMore.addEventListener("click", updateMore);
 async function updateMore(e) {
   e.preventDefault();
   let url = `https://newsapi.org/v2/top-headlines?country=us&page=2&apiKey=${API_KEY}`;
@@ -89,12 +138,13 @@ async function updateMore(e) {
   const data = await result.json();
   let newArray = data.articles;
 
-  newArray = newArray.map(x => ({...x, Checked: true}));
+  newArray = newArray.map((x) => ({ ...x, Checked: true }));
   newsArticles.push(...newArray);
-
+  numOfArticle = newsArticles.length;
+  document.getElementById("numOfArticle").innerHTML = ` (${numOfArticle})`; //load number of article
   loadMore.style.visibility = "hidden";
 
-  updateSourceObj();
+  /* updateSourceObj(); */
   render();
   renderCheckBoxArea();
 }
